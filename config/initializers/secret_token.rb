9606908592b7a7cab18176248fb4b247aeb5492f
Roman <gchrome@gmail.com>
2012-07-11 16:56:18 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chrapp::Application.config.secret_token = 'f8276363ea127487d1caf8c97636f5e6a519b94a2d6e6d554be012aa9a5fdb2137c0d21e0f8dd41ffb3b3a7c9d9674cb3bcb2ada80d1954bccd2fba52dd9fa31'
